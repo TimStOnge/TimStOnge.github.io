4b38587c0f0260a324c7452ba097dfbb686e78c8
Tim StO <timst.onge8@gmail.com>
2019-08-20 18:01:14 -0400

    Reduced header image size

--- assets/js/template.js
+++ assets/js/template.js
@@ -15,16 +15,6 @@ jQuery(document).ready(function($) {
 			my_nav.removeClass( 'stick' );
 		}   
 	};
-
-	//var initio_parallax_animation = function() { 
-	//	$('.parallax').each( function(i, obj) {
-	//		var speed = $(this).attr('parallax-speed');
-	//		if( speed ) {
-	//			var background_pos = '-' + (window.pageYOffset / speed) + "px";
-	//			$(this).css( 'background-position', 'center ' + background_pos );
-	//		}
-	//	});
-	//}
 	
 	// run our function on load
 	sticky_navigation();
